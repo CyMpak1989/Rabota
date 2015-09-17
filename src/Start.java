8b332a09068d3c44b6eca45ad7bbb2ef3cf81311
CyMpak1989 <cympak2009@mail.ru>
2015-09-17 15:35:37 +0300

    Будем делать ввод времени с клавиатуры.

--- src/Start.java
+++ src/Start.java
@@ -1,14 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Date;
 
 /**
  * Created by CyMpak on 17.09.2015.
  */
 public class Start {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Calculation a = new Calculation();
-
         System.out.println("Программа подсчета времени!");
         System.out.println();
+
+        //BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        //String name = reader.readLine();
+
         a.setStartTime(8, 0);
         a.setEndTime(16, 15);
         System.out.println(a.getStartTime());
